df8a58eea5691dcd75d2c0008722934c33feb7d3
Ckau <32091297+CkauNui@users.noreply.github.com>
2025-07-11 11:52:34 +0300

    add weak/strong blur effect

---
+++ _inc/fx/gaussianblur.glsl
@@ -0,0 +1,94 @@
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying 
+#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform   mat4 MVPMatrix;
+COMPAT_ATTRIBUTE vec2 VertexCoord;
+COMPAT_ATTRIBUTE vec2 TexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_VARYING   vec2 v_tex;
+COMPAT_VARYING   vec4 v_col;
+
+void main(void)                                    
+{                                                  
+	gl_Position = MVPMatrix * vec4(VertexCoord.xy, 0.0, 1.0);
+	v_tex       = TexCoord;                           
+	v_col       = COLOR;                           
+}
+
+#elif defined(FRAGMENT)
+			
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_VARYING   vec4      v_col;
+COMPAT_VARYING   vec2      v_tex;
+
+uniform   sampler2D u_tex;
+uniform   COMPAT_PRECISION vec2      resolution;
+uniform   COMPAT_PRECISION vec2      textureSize;
+
+void main()
+{
+	float Pi = 6.28318530718; // Pi*2
+    
+    // GAUSSIAN BLUR SETTINGS {{{
+    float Directions = 16.0; // BLUR DIRECTIONS (Default 16.0 - More is better but slower)
+    float Quality = 4.0; // BLUR QUALITY (Default 4.0 - More is better but slower)
+    float Size = 32; // BLUR SIZE (Radius)
+    // GAUSSIAN BLUR SETTINGS }}}
+   
+    vec2 Radius = Size / resolution;
+    
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = v_tex;
+    // Pixel colour
+    vec4 Color = COMPAT_TEXTURE(u_tex, uv);
+    
+    // Blur calculations
+    for( float d=0.0; d<Pi; d+=Pi/Directions)
+    {
+		for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality)
+        {
+			Color += COMPAT_TEXTURE( u_tex, uv+vec2(cos(d),sin(d))*Radius*i);		
+        }
+    }
+    
+    // Output to screen
+    Color /= Quality * Directions - 15.0;
+    FragColor =  Color * v_col;
+
+}
+#endif
